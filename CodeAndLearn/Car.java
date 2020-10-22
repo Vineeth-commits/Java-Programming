68f1f100d03f50cefd37a75b1c21734ec34c2cd1
Vineeth-forks <smoggysizzy@protonmail.com>
2020-10-23 03:32:53 +0530

    added new files

--- CodeAndLearn/Car.java
+++ CodeAndLearn/Car.java
@@ -4,6 +4,18 @@ public class Car {
     private String engine;
     private String color;
 
+    //Constructor is used to initialise the instance variables
+    public Car() {
+        // If a empty constructor is called, default values can be added by the following statement.
+        this("S-class","V8","white");
+        // The above statement must be in the first line of the constructor.
+    }
+    public Car(String model,String engine,String color) {
+        // Other methods can be called using the constructor, but a general rule dont. Use it to initialise directly
+        this.model = model;
+        this.engine = engine;
+        this.color = color;
+    }
     public void setModel(String model) {
         String validModel = model.toLowerCase();
         if(validModel.equals("Raptor"))
--- CodeAndLearn/OOP.java
+++ CodeAndLearn/OOP.java
@@ -4,10 +4,11 @@ public class OOP {
     public static void main(String[] args) {
         Car ford; //declaration 
         ford = new Car(); // initialization
-        Car Merc = new Car();
+        Car Merc = new Car("Mayback","V12","black");
         // ford = null;
         System.out.println("Model is "+ford.getModel());
         ford.setModel("Raptor");
         System.out.println("Model is "+ford.getModel());
+        Merc.getEngine();
     }
 }
